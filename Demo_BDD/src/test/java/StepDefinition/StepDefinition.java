3e0f3546410e30154d74abe62f0847e86a3cdb04
IBM <IBM@DEVOPS_10>
2018-07-25 14:41:23 +0530

    snet

--- Demo_BDD/src/test/java/StepDefinition/StepDefinition.java
+++ Demo_BDD/src/test/java/StepDefinition/StepDefinition.java
@@ -1,12 +1,15 @@
 package StepDefinition;
 
 import org.junit.Assert;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
 
 public class StepDefinition {
 	WebDriver driver;
---
+++ test/src/test/java/test/Test_Runner.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test_Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("is it working");
+
+	}
+
+}
